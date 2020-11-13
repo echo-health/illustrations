0ed17353145c5c57689a5a0d6137cc0d9d339626
Sam King <mail@samking.co>
2020-11-13 18:07:02 +0000

    Remove alt text because illustrations are decorative elements

--- build.js
+++ build.js
@@ -37,9 +37,6 @@ function format(code, format) {
   });
 }
 
-const altTextFile = fs.readFileSync("./alt-text.json", "utf8");
-const altTextMap = JSON.parse(altTextFile);
-
 let imageMap = {};
 
 function successLog(message) {
@@ -50,7 +47,6 @@ function generateWebFiles(sourcePath, imageData, resolution, width, height) {
   const name = getIllustratioNameFromFilename(sourcePath);
   const contentHash = crypto.createHash("sha1").update(imageData).digest("hex");
   const outputFileName = `${name}.${contentHash}${resolution}.png`;
-  const altText = altTextMap[name];
 
   imageMap = {
     ...imageMap,
@@ -58,7 +54,7 @@ function generateWebFiles(sourcePath, imageData, resolution, width, height) {
       ...imageMap[name],
       width: width,
       height: height,
-      altText: `Illustration of ${altText}`,
+      altText: "",
       [resolution]: `https://storage.googleapis.com/echo-illustrations/${outputFileName}`
     }
   };
